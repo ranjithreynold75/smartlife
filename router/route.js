2c51c920716a57b40f4d2258825504b53d2a7868
ranjith <ranjithreynold@gmail.com>
2017-03-10 23:20:30 -0800

    post and get request for arduino

--- router/route.js
+++ router/route.js
@@ -7,15 +7,15 @@ var path=require('path');
 module.exports=function(app){
 
 
-    app.post("/connect",function(req,res){
-        console.log("User entered"+req.body.id);
-
-
+    app.post("/handshake",function(req,res){
+        console.log("User entered"+req.body.name);
+        res.send("hai");
     })
 
-app.get("/",function(req,res){
+app.get("/handshake",function(req,res){
     //res.sendFile(path1.join(__dirname,'../public','index.html'));
-    res.sendFile(path.join(__dirname,'../html','/index.html'));
+    console.log("request been made from arduino "+req.query.name);
+res.send("hai");
 
 })
 

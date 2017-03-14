63f2c4dbbfcbd7987c1225d9ec4201a66ea7767e
ranjith <ranjithreynold@gmail.com>
2017-03-14 03:09:10 -0700

    basic

--- bin/app.js
+++ bin/app.js
@@ -1,89 +1,36 @@
 var express=require('express');
-var bodyparser=require('body-parser');
 var app=express();
-var http=require('http').Server(app);
-var io=require('socket.io')(http);
+var http=require('http').server(app);
 var m=require('mongodb');
-var url="mongodb://@localhost:27017/socket";
+var io=require('socket.io')(http);
+//var url="mongodb://@localhost:27017/jatapp";
+var url="mongodb://jatters:alwaysforward1.@ds058579.mlab.com:58579/jatapp";
 var mc=m.MongoClient;
 var _db;
-
+var bodyparser=require('body-parser')
 mc.connect(url,function(err,db){
-    _db=db;
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        _db=db;
+    }
 });
 
-function notify(req,res,next) {
-    console.log("request been made"+req.url);
-    next();
+
+function notify(req,res,next){
+console.log("request to "+req.url);
+next();
 }
-app.use(express.static(__dirname+'/html'));
+
 app.use(notify);
+app.use(bodyparser.urlencoded({extended:false}));
 
-require('../router/route.js')(app);
-
-
-
-var users={user:{
-
-}};
-io.on('connection',function(socket){
-    console.log('A user connected  '+socket.id);
-    socket.emit('message',{'id':socket.id});
-
-
-socket.on('disconnect',function(){
-    console.log('A user disconnected  '+socket.id);
-
-});
-    socket.on('connect_failed',function(){
-        console.log("A user connection error"+socket.id);
-    })
-
-        socket.on('reconnect',function(){
-        console.log("A user reconnect"+socket.id);
-    })
-
-    socket.on("register",function(data){
-        var d=JSON.parse(data);
-        console.log("register request "+d.id);
-        var a=d.no;
-        var b=d.id;
-        users.user[a]=b;
-console.log(users);
-
-
-    });
-
-
-    socket.on('message',function(data){
-
-        console.log("A user message received"+socket.id+" "+data);
-var d=JSON.parse(data);
-        var to1=d.to;
-        var from=d.from;
-        var message=d.message;
-        console.log(data);
-
-        console.log(users.user[from]);
-        if(users.user[to1]) {
-            var s=users.user[to1];
-            console.log(s);
-            io.to(s).emit("receive", {from: from, message: message});
-        }
-        else
-        {
-            //socket =users.user[from];
-            //s.emit("receive", {from: to, message: "not Online"});
-        socket.emit("receive",{from: to1, message: "not Online"});
-        }
-
-
-    })
-});
-
-
-
+require('../router/route')(app,io,_db);
 
 var server=http.listen(process.env.PORT || 8000,function(){
     console.log("server running in port 8000");
 });
+
--- router/route.js
+++ router/route.js
@@ -1,22 +1,45 @@
 
-var path=require('path');
+
+
+var users={
+    user:{
+
+    }
+
+};
+
+module.exports=function(app,io,_db){
+
+
+
+    io.on("connection",function(socket){
+
+        console.log("A user connected:"+socket.id);
+        socket.emit('id',{'id':socket.id});
+
+        socket.on('disconnect',function(){
+            console.log('A user disconnected '+socket.id);
+        })
+
+        socket.on("register",function(data){
+           var d=JSON.parse(data);
+            console.log("registering user "+d.id);
+        users.user[d.no]=d.id;
+
+        })
 
 
 
 
-module.exports=function(app){
 
 
-    app.post("/handshake",function(req,res){
-        console.log("User entered"+req.body.name);
-        res.send("hai");
+
     })
 
-app.get("/handshake",function(req,res){
-    //res.sendFile(path1.join(__dirname,'../public','index.html'));
-    console.log("request been made from arduino "+req.query.name);
-res.send("hai");
 
-})
+
+
+
+
 
 }

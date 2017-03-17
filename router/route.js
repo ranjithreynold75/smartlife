c9c5ea0ab0f5c450e5f3a9fb4adf4a5d076c84b5
ranjith <ranjithreynold@gmail.com>
2017-03-17 14:25:05 -0700

    socket changes

--- router/route.js
+++ router/route.js
@@ -2,6 +2,7 @@ var schedule=require('node-schedule');
 
 var date=new Date(2017,2,16,13,15,0);
 
+
 var users={
     user:{
 
@@ -15,8 +16,8 @@ rule.second = 5;
 module.exports=function(app,io){
 
     var j=schedule.scheduleJob(rule, function(){
-        io.sockets.emit("notify",{"message":"welcome to smartlife"});
-        // io.to(users.user['8754623583']).emit("notify", {message:"welcome to smarlife"});
+       // io.sockets.emit("notify",{"message":"welcome to smartlife"});
+         io.to(users.user['8754623583']).emit("notify", {message:"welcome to smarlife"});
         console.log("SEND notification "+date);
 
     });

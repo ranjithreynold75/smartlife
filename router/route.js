0fdc4828ef97e527755e086afcf7f2209329cc16
ranjith <ranjithreynold@gmail.com>
2017-03-24 07:31:39 -0700

    door_change

--- router/route.js
+++ router/route.js
@@ -277,10 +277,11 @@ app.get("/gas_leakage",function(req,res){
 
     //io.to(users.user[house]).emit("notify", {message:"Gas leakage detected and prevented"});
     io.sockets.in("room-"+house).emit('notify',{"message":"Gas leakage detected and prevented"});
-    res.send('{status:"Y"}');
 
     var db=_db.collection("house");
-     db.updateOne({_id:house},{$set:{door:"N"}});
+    db.updateOne({_id:house},{$set:{door:"N"}});
+    res.send('{status:"Y"}');
+
 
     //collection.find({_id:q_id},{_id:0,students:1}).toArray(function (err,d) {
 

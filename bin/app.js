ed1db1e7f28bfd4867c93076d67d16b05f9f8145
ranjith <ranjithreynold@gmail.com>
2017-05-31 13:17:55 -0700

    Profile pic update

--- bin/app.js
+++ bin/app.js
@@ -13,7 +13,7 @@ next();
 }
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(notify);
-app.use(express.static(path.join(__dirname+'/profile')));
+app.use(express.static(path.join(__dirname+'../profile')));
 
 
 

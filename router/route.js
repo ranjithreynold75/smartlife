9f9d326c9298e316304af6f738073b3c47869c3f
ranjith <ranjithreynold@gmail.com>
2017-06-05 15:54:17 -0700

    notification alert

--- router/route.js
+++ router/route.js
@@ -90,6 +90,7 @@ module.exports=function(app,io){
 
    socket.on('rasp',function(data){
 var message=JSON.parse(data);
+     console.log(message);
        io.sockets.in("room-"+8754623583).emit('notify',{"message":message.message});
 
 
